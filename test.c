d4fc682e44024a587ba3fa53c6231f9c45d1ec7a
pali3976 <patric.b.lind@gmail.com>
2014-09-30 20:29:41 +0200

    Nätverksfilsläsning, ej komplett
    La till en funktion som läser nätverksfilen

--- test.c
+++ test.c
@@ -1,17 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+void removeChars(int number, char *buffer){
+  memmove(buffer, buffer+number, strlen(buffer));  
+}
+
 int main(void){
 
   int comma = 0;
-  char buffer[] = "11, HRAJSFNKSANFASFNA";
+  char buffer[] = "1321, HRAJSFNKSANFASFNA, 33";
   char buss[10];
+  char stat[128];
   int i = 0;
-  while(comma == 0, i++){
+  int x;
+ 
+  while(comma == 0 || comma == '\0'){
     if (buffer[i] == ','){
+      memcpy(buss, buffer, i);
+      buss[i] = '\0';
+      removeChars(i+2, buffer);
       comma = 1;
-      fgets(buss, i, buffer);
+      i = 0;
     }
-  }
-  printf("Swag %d", buss);
+    else ++i;
+  } 
+  
+  while(comma == 1){
+    if (buffer[i] == ','){
+      memcpy(stat, buffer, i);
+      stat[i] = '\0';
+      removeChars(i+2, buffer);
+      i = 0;
+      comma = 2;
+    }
+    
+    else  ++i;
+  } 
+
+  x = atoi(buffer);
+  printf("%d\n", x);
+  printf("%s\n", buss);
+  printf("%s\n", stat);
+  printf("%s\n", buffer);
   return 0;
 }
